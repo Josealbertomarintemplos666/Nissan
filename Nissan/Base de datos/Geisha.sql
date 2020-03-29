d0989c445068eb2f54f8dfc54e26173e70f23cd0
ALBERTO MARIN <martin_379310@hotmail.com>
2020-03-28 22:09:58 -0600

    Proyecto inicial con base de datos

---
+++ Nissan/Base de datos/Geisha.sql
@@ -0,0 +1,79 @@
+create database Geisha
+use Geisha
+
+create table usuarios(
+idusuario int identity(1,1) primary key,
+usuario varchar(30) not null,
+contraseña varchar(30) not null,
+rol varchar(30) not null
+)
+
+create table datos(
+idvin int identity(1,1) primary key,
+descripcion varchar(60) not null,
+cuota int not null,
+cabecera varchar(6) not null,
+vin varchar(18) not null,
+motor varchar(12) not null,
+colext varchar(3) not null
+)
+
+create table clientes(
+idcliente int identity(1,1) primary key,
+nombre varchar(30) not null,
+apellidop varchar(30) not null,
+apellidom varchar(30) not null,
+direccion varchar(30) not null,
+correo varchar(30) not null,
+numero varchar(15) not null,
+)
+
+create table ventas(
+idventa int identity(1,1) primary key,
+idusuario int not null,
+idvin int not null,
+idcliente int not null,
+fechapromesa date not null,
+foreign key (idusuario) references usuarios(idusuario),
+foreign key (idvin) references datos(idvin),
+foreign key (idcliente) references clientes(idcliente)
+)
+
+create table accesorios(
+idaccesorio int identity(1,1) primary key,
+idvin int not null,
+accesorio varchar(60) not null,
+estatus varchar(30) not null,
+fechapromesa date not null,
+foreign key (idvin) references datos(idvin)
+)
+
+create table tramites(
+idtramite int identity(1,1) primary key,
+idvin int not null,
+tramite varchar(60) not null,
+estatus varchar(30) not null,
+fechapromesa date not null,
+foreign key (idvin) references datos(idvin)
+)
+
+create table preparacion(
+idpreparacion int identity(1,1) primary key,
+idvin int not null,
+preparacion varchar(60) not null,
+estatus varchar(30) not null,
+fechapromesa date not null,
+foreign key (idvin) references datos(idvin)
+)
+create table estatusfinal(
+idestatus int identity(1,1) primary key,
+idvin int not null, 
+estatus varchar(30) not null, 
+porcentaje int not null
+foreign key (idvin) references datos(idvin)
+)
+
+
+
+insert into usuarios values('Danis','danis123','admin');
+insert into usuarios values('Omar','omar123','apv');
---
+++ Nissan/Base de datos/Login.sql
@@ -0,0 +1,8 @@
+create database LoginNissan
+use LoginNissan
+
+create table Usuario (
+NUser varchar(40) primary key not null,
+Contraseña  varchar(8),
+Tipo varchar(60)
+)
