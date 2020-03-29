afafc1da7425cb90a3ec9121954d85c0108e0e9e
ALBERTO MARIN <martin_379310@hotmail.com>
2020-03-28 20:42:11 -0600

    Primer paso

---
+++ Nissan/Login.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nissan
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Cerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Mini_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Usuario_Enter(object sender, EventArgs e)
+        {
+            if (Usuario.Text == "Usuario")
+            {
+                Usuario.Text = "";
+                Usuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void Usuario_Leave(object sender, EventArgs e)
+        {
+            if (Usuario.Text == "")
+            {
+                Usuario.Text = "Usuario";
+                Usuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (PASS.Text == "Contraseña")
+            {
+                PASS.Text = "";
+                PASS.ForeColor = Color.Black;
+                PASS.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void PASS_Leave(object sender, EventArgs e)
+        {
+            if (PASS.Text == "")
+            {
+                PASS.Text = "Contraseña";
+                PASS.ForeColor = Color.Black;
+                PASS.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+
+        {
+            if (PASS.Text != "Contraseña") { 
+            if (checkBox1.Checked == false)
+
+                PASS.UseSystemPasswordChar = true;
+
+            else
+                PASS.UseSystemPasswordChar = false;
+        }
+        }
+
+    }
+}
---
+++ Nissan/Login.designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Nissan
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.Mini = new System.Windows.Forms.Button();
+            this.Cerrar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Usuario = new System.Windows.Forms.TextBox();
+            this.PASS = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(8)))), ((int)(((byte)(48)))));
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(611, 110);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.label1);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(129, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(342, 110);
+            this.panel6.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(104, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(471, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(140, 110);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.Mini);
+            this.panel5.Controls.Add(this.Cerrar);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(140, 29);
+            this.panel5.TabIndex = 0;
+            // 
+            // Mini
+            // 
+            this.Mini.BackColor = System.Drawing.Color.Transparent;
+            this.Mini.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Mini.BackgroundImage")));
+            this.Mini.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Mini.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Mini.FlatAppearance.BorderSize = 0;
+            this.Mini.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Mini.Location = new System.Drawing.Point(78, 0);
+            this.Mini.Name = "Mini";
+            this.Mini.Size = new System.Drawing.Size(31, 29);
+            this.Mini.TabIndex = 1;
+            this.Mini.UseVisualStyleBackColor = false;
+            this.Mini.Click += new System.EventHandler(this.Mini_Click);
+            // 
+            // Cerrar
+            // 
+            this.Cerrar.BackColor = System.Drawing.Color.Transparent;
+            this.Cerrar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Cerrar.BackgroundImage")));
+            this.Cerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Cerrar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Cerrar.FlatAppearance.BorderSize = 0;
+            this.Cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Cerrar.Location = new System.Drawing.Point(109, 0);
+            this.Cerrar.Name = "Cerrar";
+            this.Cerrar.Size = new System.Drawing.Size(31, 29);
+            this.Cerrar.TabIndex = 0;
+            this.Cerrar.UseVisualStyleBackColor = false;
+            this.Cerrar.Click += new System.EventHandler(this.Cerrar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(129, 110);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gray;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 110);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(129, 259);
+            this.panel3.TabIndex = 2;
+            // 
+            // Usuario
+            // 
+            this.Usuario.BackColor = System.Drawing.SystemColors.Control;
+            this.Usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Usuario.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usuario.Location = new System.Drawing.Point(172, 164);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(225, 24);
+            this.Usuario.TabIndex = 3;
+            this.Usuario.Text = "Usuario";
+            this.Usuario.Enter += new System.EventHandler(this.Usuario_Enter);
+            this.Usuario.Leave += new System.EventHandler(this.Usuario_Leave);
+            // 
+            // PASS
+            // 
+            this.PASS.BackColor = System.Drawing.SystemColors.Control;
+            this.PASS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PASS.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PASS.Location = new System.Drawing.Point(172, 233);
+            this.PASS.Name = "PASS";
+            this.PASS.Size = new System.Drawing.Size(225, 24);
+            this.PASS.TabIndex = 4;
+            this.PASS.Text = "Contraseña";
+            this.PASS.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.PASS.Leave += new System.EventHandler(this.PASS_Leave);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(428, 193);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(162, 25);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Mirar contraseña";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(240, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 35);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(611, 369);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.PASS);
+            this.Controls.Add(this.Usuario);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button Mini;
+        private System.Windows.Forms.Button Cerrar;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Usuario;
+        private System.Windows.Forms.TextBox PASS;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
