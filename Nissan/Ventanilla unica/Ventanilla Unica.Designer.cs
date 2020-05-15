8ea8d2dd15d80db7acde31939e159854306a0add
Omar Muñoz Vazquez <omar-luks@hotmail.com>
2020-05-15 16:02:40 -0500

    Boton volver

--- Nissan/Ventanilla unica/Ventanilla Unica.Designer.cs
+++ Nissan/Ventanilla unica/Ventanilla Unica.Designer.cs
@@ -28,13 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(286, 388);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Volver";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Ventanilla_unica
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Ventanilla_unica";
             this.Text = "Ventanilla_unica";
             this.Load += new System.EventHandler(this.Ventanilla_unica_Load);
@@ -43,5 +57,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
