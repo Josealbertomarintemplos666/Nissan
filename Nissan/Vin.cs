8729908cdc364536a289131fb2b286a1c2b6d291
Omar Muñoz Vazquez <omar-luks@hotmail.com>
2020-05-21 17:05:56 -0500

    Clase Vin

---
+++ Nissan/Vin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nissan
+{
+    public class Vin
+    {
+        public String vin { get; set; }
+        public String descripcion { get; set; }
+        public String cuota { get; set; }
+        public String cabecera { get; set; }
+        public String motor { get; set; }
+        public String colext { get; set; }
+
+    }
+}
