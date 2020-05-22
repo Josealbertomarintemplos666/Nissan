f2627240aca86dee0a080f4f31e28b5b420aa80f
ALBERTO MARIN <martin_379310@hotmail.com>
2020-05-21 23:28:53 -0500

    Cambios  10

--- Nissan/Administrador/CargaDatos.cs
+++ Nissan/Administrador/CargaDatos.cs
@@ -30,10 +30,7 @@ namespace Nissan
 
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
+       
 
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
