b92dc5abf0a7773bf6dcd53654b759066f51b91d
Omar Muñoz Vazquez <omar-luks@hotmail.com>
2020-05-23 20:58:02 -0500

    Pan

--- Nissan/Administrador/InsertarUsuario.Designer.cs
+++ Nissan/Administrador/InsertarUsuario.Designer.cs
@@ -28,126 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.Usuario = new System.Windows.Forms.TextBox();
-            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
-            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.button1 = new System.Windows.Forms.Button();
-            this.panel2.SuspendLayout();
+            this.PASS = new System.Windows.Forms.TextBox();
+            this.Usuario = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.Gray;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(217, 423);
-            this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(8)))), ((int)(((byte)(48)))));
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Controls.Add(this.Usuario);
-            this.panel2.Controls.Add(this.shapeContainer1);
-            this.panel2.Location = new System.Drawing.Point(217, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(697, 423);
-            this.panel2.TabIndex = 1;
-            // 
-            // Usuario
-            // 
-            this.Usuario.BackColor = System.Drawing.Color.White;
-            this.Usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.Usuario.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Usuario.Location = new System.Drawing.Point(87, 121);
-            this.Usuario.Name = "Usuario";
-            this.Usuario.Size = new System.Drawing.Size(225, 24);
-            this.Usuario.TabIndex = 4;
-            this.Usuario.Text = "Nombres";
-            // 
-            // lineShape1
-            // 
-            this.lineShape1.BorderWidth = 2;
-            this.lineShape1.Name = "lineShape1";
-            this.lineShape1.X1 = 87;
-            this.lineShape1.X2 = 315;
-            this.lineShape1.Y1 = 146;
-            this.lineShape1.Y2 = 146;
-            // 
-            // shapeContainer1
-            // 
-            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
-            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
-            this.shapeContainer1.Name = "shapeContainer1";
-            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
-            this.lineShape2,
-            this.lineShape1});
-            this.shapeContainer1.Size = new System.Drawing.Size(697, 423);
-            this.shapeContainer1.TabIndex = 5;
-            this.shapeContainer1.TabStop = false;
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(87, 171);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(225, 24);
-            this.textBox1.TabIndex = 6;
-            this.textBox1.Text = "Agentes";
-            // 
-            // lineShape2
-            // 
-            this.lineShape2.BorderWidth = 2;
-            this.lineShape2.Name = "lineShape2";
-            this.lineShape2.X1 = 86;
-            this.lineShape2.X2 = 314;
-            this.lineShape2.Y1 = 196;
-            this.lineShape2.Y2 = 196;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(8)))), ((int)(((byte)(48)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(229, 280);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(353, 262);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(278, 35);
-            this.button1.TabIndex = 8;
+            this.button1.TabIndex = 10;
             this.button1.Text = "Registrar";
             this.button1.UseVisualStyleBackColor = false;
+            // 
+            // PASS
+            // 
+            this.PASS.BackColor = System.Drawing.SystemColors.Control;
+            this.PASS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PASS.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PASS.Location = new System.Drawing.Point(285, 197);
+            this.PASS.Name = "PASS";
+            this.PASS.Size = new System.Drawing.Size(225, 24);
+            this.PASS.TabIndex = 9;
+            this.PASS.Text = "Agente";
+            // 
+            // Usuario
+            // 
+            this.Usuario.BackColor = System.Drawing.SystemColors.Control;
+            this.Usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Usuario.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usuario.Location = new System.Drawing.Point(285, 132);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(225, 24);
+            this.Usuario.TabIndex = 8;
+            this.Usuario.Text = "Nombres";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(238, 423);
+            this.panel1.TabIndex = 0;
             // 
             // InsertarUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
             this.ClientSize = new System.Drawing.Size(916, 425);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PASS);
+            this.Controls.Add(this.Usuario);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "InsertarUsuario";
             this.Text = "InsertarUsuario";
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.TextBox Usuario;
-        private System.Windows.Forms.TextBox textBox1;
-        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
-        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
-        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox PASS;
+        private System.Windows.Forms.TextBox Usuario;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Nissan/Administrador/InsertarUsuario.cs
+++ Nissan/Administrador/InsertarUsuario.cs
@@ -16,5 +16,10 @@ namespace Nissan.Administrador
         {
             InitializeComponent();
         }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
