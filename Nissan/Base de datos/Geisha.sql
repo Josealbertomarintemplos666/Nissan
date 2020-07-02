3b44012f850dce740aa98e295fe29708b7689c9e
Omar Muñoz Vazquez <omar-luks@hotmail.com>
2020-07-01 20:05:03 -0500

    base

--- Nissan/Base de datos/Geisha.sql
+++ Nissan/Base de datos/Geisha.sql
@@ -14,62 +14,49 @@ motor varchar(25) not null,
 colext varchar(25) not null,
 estatus varchar(25)check(estatus='Entregado' or estatus='En proceso' or estatus='No iniciado')default'No iniciado'not null
 )
+
+-------------------------
+
 create table Ventanilla(
 idventanilla int identity(1,1) primary key,
-nombre varchar(50) not null
+nombre varchar(80) unique not null,
+contraseña varchar(30) not null,
+roles varchar(30) not null
 )
+
 create table Asesor(
-idapv int identity(1,1) primary key,
-nombres varchar(80),
-agente int
+idapv int primary key,
+nombres varchar(80) unique not null,
+contraseña varchar(30) not null,
+roles varchar(30) not null
 )
 
-
-create table Tramite(
-nombre varchar(100) primary key,
-estatus varchar(20)check(estatus='Terminado' or estatus='En proceso' or estatus='No iniciado')default'No iniciado' not null,
-idventanilla int not null,
-encargado varchar(50) not null,
-fecha date not null,
-vin varchar(25) not null,
-observacion varchar(100),
-foreign key (vin) references Vin(vin),
-foreign key (idventanilla) references Ventanilla(idventanilla)
+create table OperadorVentanilla(
+idope int identity(1,1) primary key,
+nombres varchar(80) unique not null,
+contraseña varchar(30) not null,
+roles varchar(30) not null
 )
 
-create table Accesorios(
-idaccesorio int identity(1,1) primary key,
-vin varchar(25) not null,
-accesorio varchar(60) not null,
-idventanilla int not null,
-encargado varchar(50) not null,
-estatus varchar(30) check(estatus='Terminado' or estatus='En proceso' or estatus='No iniciado') default'No iniciado' not null,
-fechapromesa date not null,
-observacion varchar(100),
-foreign key (vin) references Vin(vin),
-foreign key (idventanilla) references Ventanilla(idventanilla)
+create table SAccesorios(
+idaccesorios int identity(1,1) primary key,
+nombres varchar(80) unique not null,
+contraseña varchar(30) not null,
+roles varchar(30) not null
 )
 
-create table Preparacion(
-preparacion varchar(60) primary key,
-vin varchar(25) not null,
-idventanilla int not null,
-encargado varchar(50) not null,
-estatus varchar(30)check(estatus='Terminado' or estatus='En proceso' or estatus='No iniciado')default'No iniciado' not null,
-fechapromesa date not null,
-observacion varchar(100),
-foreign key (vin) references Vin(vin),
-foreign key (idventanilla) references Ventanilla(idventanilla)
+create table Tramites(
+idtramites int identity(1,1) primary key,
+nombres varchar(80) unique not null,
+contraseña varchar(30) not null,
+roles varchar(30) not null
 )
 
-create table Venta(
-idventa varchar(60) primary key,
-idapv int not null,
-vin varchar(25) not null,
-fechapromesa date not null,
-clente varchar(100) not null,
-foreign key (idapv) references Asesor(idapv),
-foreign key (vin) references Vin(vin)
+create table Operarios(
+idoperarios int identity(1,1) primary key,
+nombres varchar(80) unique not null,
+contraseña varchar(30) not null,
+roles varchar(30) not null
 )
 
 create table sesion(
@@ -83,20 +70,72 @@ fecha date primary key,
 usuario varchar(30) not null,
 foreign key (usuario) references sesion(Usuario)
 )
+-----------------------------------------
+create table Tramite(
+nombre varchar(100) primary key,
+estatus varchar(20)check(estatus='Terminado' or estatus='En proceso' or estatus='No iniciado')default'No iniciado' not null,
+idope int not null,
+encargado varchar(50) not null,
+fecha date not null,
+vin varchar(25) not null,
+observacion varchar(100),
+foreign key (vin) references Vin(vin),
+foreign key (idope) references OperadorVentanilla(idope)
+)
 
---Inserts
+create table Accesorios(
+idaccesorio int identity(1,1) primary key,
+vin varchar(25) not null,
+accesorio varchar(60) not null,
+idope int not null,
+encargado varchar(50) not null,
+estatus varchar(30) check(estatus='Terminado' or estatus='En proceso' or estatus='No iniciado') default'No iniciado' not null,
+fechapromesa date not null,
+observacion varchar(100),
+foreign key (vin) references Vin(vin),
+foreign key (idope) references OperadorVentanilla(idope)
+)
 
+create table Preparacion(
+preparacion varchar(60) primary key,
+vin varchar(25) not null,
+idope int not null,
+encargado varchar(50) not null,
+estatus varchar(30)check(estatus='Terminado' or estatus='En proceso' or estatus='No iniciado')default'No iniciado' not null,
+fechapromesa date not null,
+observacion varchar(100),
+foreign key (vin) references Vin(vin),
+foreign key (idope) references OperadorVentanilla(idope)
+)
+
+create table Venta(
+idventa varchar(60) primary key,
+idapv int not null,
+vin varchar(25) not null,
+fechapromesa date not null,
+clente varchar(100) not null,
+foreign key (idapv) references Asesor(idapv),
+foreign key (vin) references Vin(vin)
+)
+
+
+--Inserts sesion
 insert sesion values('juan','1234','Administrador')
 insert sesion values('dani','1234','Ventanilla unica')
 insert sesion values('omar','1234','apv')
 insert sesion values('ivan','1234','gerente de ventas')
-
+insert sesion values('templos','1234','Operador Ventanilla Unica')
 --insert sesion values('jose','1234','Mecannico')
+
+--insert Operador de Ventanilla
+insert OperadorVentanilla values('templos','1234','Operador Ventanilla Unica')
+
 
 --Store procedure´s
 
 
 --Extras
+select * from OperadorVentanilla
 select * from sesion
 select * from sesion where usuario='juan' and contraseña='1234'
 
